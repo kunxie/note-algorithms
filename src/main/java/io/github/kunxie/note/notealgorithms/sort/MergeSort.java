525c608979360a20d4509b31fd9e32f1ad96c593
Kun Xie <kun.xie.000000@gmail.com>
2024-12-06 00:24:18 -0600

    add mergesort

--- src/main/java/io/github/kunxie/note/notealgorithms/sort/MergeSort.java
+++ src/main/java/io/github/kunxie/note/notealgorithms/sort/MergeSort.java
@@ -1,4 +1,38 @@
 package io.github.kunxie.note.notealgorithms.sort;
 
-public class MergeSort {
+public class MergeSort implements Sort<Integer> {
+
+  @Override
+  public void sort(Integer[] array) {
+    var helpArray = new Integer[array.length];
+    sort(array, 0, array.length - 1, helpArray);
+  }
+
+  private void sort(Integer[] array, int left, int right, Integer[] helpArray) {
+    if (left >= right) return;
+
+    var mid = (right - left) / 2 + left;
+    sort(array, left, mid, helpArray);
+    sort(array, mid + 1, right, helpArray);
+
+    var i = left;
+    var j = mid + 1;
+    var x = left;
+    while (i <= mid || j <= right) {
+      if (i > mid) {
+        helpArray[x++] = array[j++];
+      } else if (j > right) {
+        helpArray[x++] = array[i++];
+      } else if (array[i].compareTo(array[j]) < 0) {
+        helpArray[x++] = array[i++];
+      } else {
+        helpArray[x++] = array[j++];
+      }
+    }
+    assert i == mid + 1;
+    assert j == right + 1;
+    assert x == right + 1;
+
+    System.arraycopy(helpArray, left, array, left, right + 1 - left);
+  }
 }
---
+++ src/test/java/io/github/kunxie/note/notealgorithms/sort/MergeSortTest.java
@@ -0,0 +1,49 @@
+package io.github.kunxie.note.notealgorithms.sort;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+class MergeSortTest {
+
+  List<Sort<Integer>> sortMethods;
+
+  @BeforeEach
+  void init() {
+    this.sortMethods = List.of(
+        new QuickSort(),
+        new MergeSort()
+    );
+  }
+
+  @ParameterizedTest
+  @MethodSource("arrayProvider")
+  void sort(Integer[] array) {
+    for (Sort<Integer> sortMethod : sortMethods) {
+      logArray("before sort", array);
+      sortMethod.sort(array);
+      logArray("after sort", array);
+      assertTrue(SortUtils.isAscend(array));
+    }
+  }
+
+  private void logArray(String message, Integer[] array) {
+    log.info("{} -- array:{}", message, Arrays.toString(array));
+  }
+
+  static Stream<Arguments> arrayProvider() {
+    return Stream.of(
+        Arguments.of((Object) new Integer[]{1, 2, 3, 4, 5}),
+        Arguments.of((Object) new Integer[]{5, 3, 4, 1, 2, 8, 10, 6, 5})
+    );
+  }
+}
